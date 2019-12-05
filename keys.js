953b1e4bbeae41142c84c67950c1c31f6ebb322c
Matt DeMonaco <mdemonaco@gmail.com>
2019-12-04 21:21:01 -0800

    OMDB, Spotify, and Bands in Town APIs are working successfully, queries are being made.

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.bf61b427d01741b6a11b1fdcfd587d30,
-  secret: process.env.c889093e93684daaa97253b63dba82ef
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -2,71 +2,68 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
-
 var fs = require('fs');
 
 var axios = require('axios');
 
 var Spotify = require("node-spotify-api");
 
- 
-    start()
-function start() {
-    console.log("Testing 123")
-    mainMenu();
-}
+var spotify = new Spotify(keys.spotify);
 
-function mainMenu(directory, data) {
-            switch (directory) {
-                //bands in town function
-                case "Find-a-show":
-                    findShow(data);
-                    break;
-                //spotify function
-                case "Listen-to-a-song":
-                    listenSong(data);
-                    break;
-                //OMDB function
+var directory = process.argv[2];
 
-                case "Search-for-Film-or-show":
-                    omdbList(data);
-                    break;
-
-                    //default to Backstreet Boys
-                case "do-what-it-says":
-                     backstreetsback(data);
-                     break;
+var media = process.argv.slice(3).join(" ");
 
 
-                //leave
+//     start()
+// function start() {
+//     console.log("Hi welcome to your media consumption app!")
+//     mainMenu();
+// }
 
-                case "Leave":
-                    console.log("Later dude!");
+switch (directory) {
+    //bands in town function
+    case "Find-a-show":
+        findShow(media);
+        break;
+    //spotify function
+    case "spotify-this-song":
+        listenSong(media);
+        replacesong();
+        break;
+    //OMDB function
 
-            }
-        }
+    case "Search-for-Film-or-show":
+        omdbList(media);
+        mrNobody();
+        break;
 
-function bandsChoice(){
-    var findAShow = process.argv[2];
-    var show = process.argv[3];
-    var showQuery = process.argv.slice(3).join("");
-    console.log("I'm looking for " + showQuery);
+    //default to Backstreet Boys
+    case "do-what-it-says":
+        backstreetsback();
+        break;
+
+
+    //leave
+
+    case "Leave":
+        console.log("Later dude!");
 
 }
+
+
 
 //Bands in town API request
-function findShow(data){
-    
-    var artist = process.argv.slice(3).join("");
+function findShow(artist) {
+    console.log(artist)
     //API call with artist as part of the URLquery
-    var urlQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response) {
-        console.log(response.data.venue.name);
-        console.log(response.data.venue.city);
-        console.log(response.data.datetime);
-        })
-        .catch(function(error) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
+        for (var i = 0; i < response.data.length; i++)
+            console.log(response.data[i].venue);
+        console.log(response.data[i].datetime);
+
+    })
+        .catch(function (error) {
             if (error.response) {
                 console.log("--------Data-----");
                 console.log(error.response.data);
@@ -81,32 +78,17 @@ function findShow(data){
             }
             console.log(error.config);
         })
-    }
-
-//Spotify song request
-function listenSong(data){
-    var song = process.argv.slice(3).join("");
- spotify.search({ type: 'artist OR album OR track', query: song, limit: 20 }, callback);
- console.log("response.data")
 }
 
-//OMBD call
+//Spotify song request
+function listenSong(song) {
 
-function omdbList(){
-    var movieName = process.argv.slice(3).join("");
-    //API call with movie as part of the URLquery
-    var urlQuery = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=834c9300"
-    axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=834c9300").then(function(response) {
-    console.log(response.data.title);
-    console.log(response.data.year);
-    console.log(response.data.imdbRating);
-    console.log(response.data.ratings[1]);
-    console.log(response.data.country);
-    console.log(response.data.language);
-    console.log(response.data.plot);
-    console.log(response.data.actors);
-    })
-    .catch(function(error){
+    spotify.search({ type: "track", query: song, limit: 20 }).then(function (response) {
+        console.log(response.tracks.items[0].artists);
+        console.log(song);
+        console.log(response.tracks.items[0].external_urls);
+        console.log(response.tracks.items[0].album.uri);
+    }).catch(function (error) {
         if (error.response) {
             console.log("--------Data-----");
             console.log(error.response.data);
@@ -123,26 +105,57 @@ function omdbList(){
     })
 }
 
-//liri do-what-it-says
-function backstreetsback(){
-    spotify.request("https://api.spotify.com/v1/tracks/")
-    .then(function(data) {
-        console.log(data);
-    })
-    .catch(function(err) {
-        console.error('Error occured: ' + err);
-    })
-};
+    function replacesong() {
+        if (process.argv[3] == undefined);
+        // if (song == NaN || song == null || song == "" || song == undefined) {
+            listenSong("The sign ace of base")
+        }
+    
 
-//write information over to random.txt
-function saveText() {
-fs.writeFile("random.txt", userSelect, function (error, data){
+        //OMBD call
 
-    if (error) {
-        return console.log (error);
-    }
+        function omdbList(media) {
 
-    console.log(data);
-   var dataArr = data.split(",");
-})
-}
+            //API call with movie as part of the URLquery
+            axios.get("http://www.omdbapi.com/?t=" + media + "&y=&plot=short&apikey=834c9300").then(function (response) {
+                console.log(response.data.Title);
+                console.log(response.data.Year);
+                console.log(response.data.Ratings[0]);
+                console.log(response.data.Ratings[1]);
+                console.log(response.data.Country);
+                console.log(response.data.Language);
+                console.log(response.data.Plot);
+                console.log(response.data.Actors);
+            })
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log("--------Data-----");
+                        console.log(error.response.data);
+                        console.log("--------Status------");
+                        console.log(error.response.status);
+                        console.log("------Status-----");
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                })
+
+            function mrNobody() {
+                if (process.argv[3] == "undefined")
+                    mrNobody("Mr. Nobody");
+            }
+        }
+
+        //liri do-what-it-says
+        function backstreetsback() {
+            return fs.readFile("random.txt", "utf8", function (error, data) {
+                if (error) {
+                    return console.log(error);
+                }
+                // console.log(data.split(",")[1]);
+                listenSong(data.split(",")[1]);
+            })
+        }
